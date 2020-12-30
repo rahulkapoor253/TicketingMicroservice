bef60c0dfe9c39e1b335b93e0fb9620c7372e739
Rahul Kapoor <rahul253801@gmail.com>
2020-12-30 23:46:59 +0530

    added skaffold and ingress config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("My name is Rahul.");
+});
+
 app.listen(3000, () => {
-  console.log("listening on port 3000");
+  console.log("listening on port 3000.");
 });
