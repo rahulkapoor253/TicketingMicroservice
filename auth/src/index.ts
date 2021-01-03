a2fc055531e6a26ea99d37bd5235d9e76988f1e4
Rahul Kapoor <rahul253801@gmail.com>
2021-01-03 19:55:26 +0530

    abstract functionality added for custom errors, async error handling implemented

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { json } from "body-parser";
+import "express-async-errors";
 import { currentUserRouter } from "./routes/current-user";
 import { signoutRouter } from "./routes/signout";
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -14,6 +16,10 @@ app.use(signoutRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(errorHandler);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
 
 app.listen(3000, () => {
   console.log("listening on port 3000.");
